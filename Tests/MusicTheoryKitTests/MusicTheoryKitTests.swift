9e3b92d4c5f2ea9065e998553bb68c86c69f49c8
Jin Zhang <sh3l6or@icloud.com>
2021-12-13 18:31:47 -0800

    Test pass

--- Tests/MusicTheoryKitTests/MusicTheoryKitTests.swift
+++ Tests/MusicTheoryKitTests/MusicTheoryKitTests.swift
@@ -2,10 +2,12 @@ import XCTest
 @testable import MusicTheoryKit
 
 final class MusicTheoryKitTests: XCTestCase {
-    func testExample() throws {
+    func testNoteAddition() throws {
 
         XCTAssertEqual(Note.C + Interval.m2, Note.Db)
         XCTAssertEqual(Note.E + Interval.m2, Note.F)
+        XCTAssertEqual(Note.Bb + Interval.m7, Note.Ab)
+        XCTAssertEqual(Note.G + Interval.p5, Note.D)
         
     }
 }
