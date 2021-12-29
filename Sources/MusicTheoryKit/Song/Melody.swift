22caf308f68c5b23bda73db528d252bc8ef0b82d
Jin Zhang <sh3l6or@icloud.com>
2021-12-29 14:37:21 -0800

    Better define melody

--- Sources/MusicTheoryKit/Song/Melody.swift
+++ Sources/MusicTheoryKit/Song/Melody.swift
@@ -5,51 +5,22 @@
 //  Created by Jin Zhang on 12/16/21.
 //
 
-/// Monophonic melody.
-public struct Melody {
-    // Elements in the melody.
-    var elements: [MelodyElement]
-    /// Create a melody with an array of melody elements.
-    public init() {
-        self.elements = []
-    }
-    private init(_ elements: [MelodyElement]) {
-        self.elements = elements
-    }
+/// Element in a monophonic melody.
+public protocol MelodyElement {
+    /// Value of such an element.
+    var value: Value { get }
 }
+
+public typealias Melody = [MelodyElement]
 
 extension Melody {
     /// The leagth of the melody measured in beats.
     public var beats: Double {
-        elements.map { element in element.value.beats }.reduce(0, +)
+        self.map { element in element.value.beats }.reduce(0, +)
     }
     /// Add an element, either a music note or pause to this melody.
     /// - Parameter element: A music note or a pause.
     public func add(_ element: MelodyElement) -> Melody {
-        Melody(elements + [element])
-    }
-}
-
-extension Melody : Sequence {
-    /// Get an iterator of this melody.
-    /// - Returns: An iterator of this melody.
-    public func makeIterator() -> Iterator {
-        Iterator(self)
-    }
-    /// Iterator of a melody.
-    public struct Iterator : IteratorProtocol {
-        /// Create an iterator of a melody.
-        /// - Parameter melody: The melody to be iterated.
-        public init(_ melody: Melody) {
-            self.melody = melody
-        }
-        /// Returns the next element in melody not iterated.
-        /// - Returns: The next element in melody not iterated.
-        public mutating func next() -> MelodyElement? {
-            self.melody.elements.count != 0
-                ? self.melody.elements.removeFirst()
-                : nil
-        }
-        private var melody: Melody
+        Melody(self + [element])
     }
 }
